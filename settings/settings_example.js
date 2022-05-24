1579ed9f74fd913d36ac8a7134b62a16d5a3cd8a
Kaminari <jonasnbr@hiof.no>
2022-05-24 20:07:21 +0200

    init + basic settings

---
+++ settings/settings_example.js
@@ -0,0 +1,13 @@
+/** @type {credentials} */
+const creds = {
+  client_id: "",
+  client_secret: "",
+}
+
+/** @type {Settings} */
+const settings = {
+  logFilePath: "../logs/logs.log",
+  userIdToScrape: "560363490",
+}
+
+export { settings, creds }
